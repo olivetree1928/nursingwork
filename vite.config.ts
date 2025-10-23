34ff298489da94fd7be30af1a96701e7903e617b
olivetree1928 <olivetree1928@gmail.com>
2025-10-23 18:13:58 +0800

    git view problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nursingwork/', // 添加GitHub Pages的仓库名作为base路径
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
